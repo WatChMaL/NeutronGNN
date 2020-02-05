e999232df8bb829379e37717f19af79484e4158a
John Walker <jmgwalker@triumf.ca>
2020-02-04 16:02:10 -0800

    Config file to run gcn_kipf on triumf

--- GraphNets/config/config_triumf.py
+++ GraphNets/config/config_triumf.py
@@ -4,32 +4,40 @@ from config.easy_dict import EasyDict
 
 config = EasyDict()
 
-config.model_name = "encoded_cheby_highway"
-config.model_kwargs = {"k":3, "layers":20, "graph_w":256, "concat_layers":[3,5,7,10,13,16,20], "lin_ws":[64, 64]}
+## Model
+config.model_name = "gcn_kipf"
+config.model_kwargs = {"w1":4, "w2":8, "w3":16}
 
-config.data_path = "/data/WatChMaL/data/IWCDmPMT_4pi_fulltank_test/split_h5/IWCDmPMT_4pi_fulltank_test_graphnet_trainval.h5"
-config.indices_file = "/data/WatChMaL/data/IWCDmPMT_4pi_fulltank_test/split_h5/IWCDmPMT_4pi_fulltank_test_graphnet_trainval_idxs.npz"
-config.edge_index_pickle = "/data/WatChMaL/graphnets/visualization/mpmt_edges_dict.pkl"
+## Data paths
+config.data_path = "/home/jwalker/data/NeutronGNN/iwcd_mpmt_shorttank_neutrongnn_trainval.h5"
+config.indices_file = "/home/jwalker/data/NeutronGNN/iwcd_mpmt_shorttank_neutrongnn_trainval_idxs.npz"
 
-config.dump_path = "/data/WatChMaL/graphnets/GraphNets/dump/" + config.model_name
+## Log location
+config.dump_path = "/home/jwalker/data/NeutronGNN/dump/" + config.model_name
 
+## Computer parameters
 config.num_data_workers = 0 # Sometime crashes if we do multiprocessing
 config.device = 'gpu'
-config.gpu_list = [6]
+config.gpu_list = [0]
 
-config.optimizer = "SGD"
-config.optimizer_kwargs = {"lr":0.01, "weight_decay":1e-3, "momentum":0.9, "nesterov":True}
+## Optimizer parameters
+config.optimizer = "Adam"
+config.optimizer_kwargs = {"lr":0.01, "weight_decay":5e-4}
 
+## Scheduler parameters
 config.scheduler_kwargs = {"mode":"min", "min_lr":1e-6, "patience":1, "verbose":True}
 config.scheduler_step = 190
 
+## Training parameters
 config.batch_size = 64
-config.epochs = 15
+config.epochs = 10
 
+## Logging parameters for training
 config.report_interval = 50
 config.num_val_batches  = 16
 config.valid_interval   = 200
 
+## Validating parameters
 config.validate_batch_size = 64
 config.validate_dump_interval = 256
 config.use_encoded_data = True
