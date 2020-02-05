dbe15428636fe8f609cd0853d1ab12f63e66674f
John Walker <jmgwalker@triumf.ca>
2020-02-04 16:03:58 -0800

    Removing loading edges from file and fixed nodes

--- GraphNets/io_util/data_handler.py
+++ GraphNets/io_util/data_handler.py
@@ -17,12 +17,10 @@ class WCH5Dataset_trainval(WCH5Dataset):
     """
 
     def __init__(self, path, indices_file,
-                 edge_index_pickle, nodes=15808,
                  transform=None, pre_transform=None, pre_filter=None,
                  use_node_attr=False, use_edge_attr=False, cleaned=False):
 
         super(WCH5Dataset_trainval, self).__init__( path,
-                 edge_index_pickle, nodes=nodes,
                  transform=transform, pre_transform=pre_transform, pre_filter=pre_filter,
                  use_node_attr=use_node_attr, use_edge_attr=use_edge_attr, cleaned=cleaned)
 
@@ -31,9 +29,9 @@ class WCH5Dataset_trainval(WCH5Dataset):
         self.train_indices = all_indices["train_idxs"]
         self.val_indices = all_indices["val_idxs"]
 
-def get_loaders(path, indices_file, edges_dict_pickle, batch_size, workers):
+def get_loaders(path, indices_file, batch_size, workers):
 
-    dataset = WCH5Dataset_trainval(path, indices_file, edges_dict_pickle)
+    dataset = WCH5Dataset_trainval(path, indices_file)
 
     train_loader=DataLoader(dataset, batch_size=batch_size, num_workers=workers,
                             pin_memory=True, sampler=SubsetRandomSampler(dataset.train_indices))
@@ -44,13 +42,14 @@ def get_loaders(path, indices_file, edges_dict_pickle, batch_size, workers):
     return train_loader, val_loader, dataset
 
 
-def get_loaders_encoded(path, indices_file, edges_dict_pickle, batch_size, workers):
+def get_loaders_encoded(path, indices_file, batch_size, workers):
     
-    dataset = WCH5Dataset_trainval(path, indices_file, edges_dict_pickle,
-                                nodes=832, transform=transform_reshape((832,38)))
+    dataset = WCH5Dataset_trainval(path, indices_file)
+
     train_loader=DataLoader(dataset, batch_size=batch_size, num_workers=workers,
                             pin_memory=True, sampler=SubsetRandomSampler(dataset.train_indices))
 
     val_loader=DataLoader(dataset, batch_size=batch_size, num_workers=workers,
                             pin_memory=True, sampler=SubsetRandomSampler(dataset.val_indices))
+    
     return train_loader, val_loader, dataset
